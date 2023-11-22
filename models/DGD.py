818cfe8689c5a8c458ed75d2b76c6795b2c07171
SMRhadou <s.elaraby.user@gmail.com>
2023-11-22 09:56:20 -0500

    Add decentralized benchmarks

--- models/DGD.py
+++ models/DGD.py
@@ -28,11 +28,60 @@ def DGD(dataset, Graph, inner_loss, criterion, **kwargs):
     # Forward path
     acc = []
     acc.append(accuracy(y, dataset[1][0], dataset[1][1].long(), device).item())
-    indices = np.arange(dataset[1][0].shape[1])
+    indices = np.random.randint(0, dataset[0][0].shape[1])
     for l in range(kwargs['nEpochs']):
         y1 = Graph @ y
-        y2 = compute_grad(y, inner_loss, dataset[1], indices, device)
+        y2 = compute_grad(y, inner_loss, dataset[0], indices, device)
+        y = y1 - 10*kwargs['alpha'] * y2 
+        acc.append(criterion(y, dataset[1][0], dataset[1][1].long(), device).item())
+    torch.cuda.empty_cache()
+    return y, acc
+
+def DSGD(dataset, Graph, inner_loss, criterion, **kwargs):
+    if "device" in kwargs.keys():
+        device = kwargs["device"]
+    else:
+        device = "cuda"
+
+    # Random Initialization
+    y = torch.distributions.Normal(0.0, 5).sample((kwargs['nAgents'], kwargs['LLSize'])).float().to(device)
+    
+    # Forward path
+    acc = []
+    acc.append(accuracy(y, dataset[1][0], dataset[1][1].long(), device).item())
+    indices = np.random.randint(0, dataset[0][0].shape[1])
+    for l in range(kwargs['nEpochs']):
+        y1 = Graph @ y
+        y2 = compute_grad(y, inner_loss, dataset[0], indices, device)
+        y = y1 - 100* kwargs['alpha'] * y2 
+        acc.append(criterion(y, dataset[1][0], dataset[1][1].long(), device).item())
+    torch.cuda.empty_cache()
+    return y, acc
+
+def DFedAvg(dataset, Graph, inner_loss, criterion, **kwargs):
+    if "device" in kwargs.keys():
+        device = kwargs["device"]
+    else:
+        device = "cuda"
+
+    # Random Initialization
+    y = torch.distributions.Normal(0.0, 5).sample((kwargs['nAgents'], kwargs['LLSize'])).float().to(device)
+    
+    # Forward path
+    acc = []
+    acc.append(accuracy(y, dataset[1][0], dataset[1][1].long(), device).item())
+    indices =  np.random.randint(0, dataset[0][0].shape[1], 10)
+    for l in range(kwargs['nEpochs']):
+        y1 = Graph @ y
+        y2 = compute_grad(y, inner_loss, dataset[0], indices, device)
         y = y1 - kwargs['alpha'] * y2 
+        oldy1 = y.clone()
+        oldy2 = y.clone()
+        for k in range(5):
+            dy = compute_grad(y, inner_loss, dataset[0], indices, device)
+            y = y - 1 * dy + 0.1 * (oldy1 - oldy2)
+            oldy1 = y.clone()
+            oldy2 = oldy1.clone()
         acc.append(criterion(y, dataset[1][0], dataset[1][1].long(), device).item())
     torch.cuda.empty_cache()
     return y, acc
@@ -40,10 +89,16 @@ def DGD(dataset, Graph, inner_loss, criterion, **kwargs):
 def metric(metadataset, Graph, inner_loss, criterion=accuracy, **kwargs):
     device = kwargs["device"]
     testAccuracy = np.zeros((len(metadataset), kwargs['nEpochs']+1))
+    testAccuracy_DSGD = np.zeros((len(metadataset), kwargs['nEpochs']+1))
+    testAccuracy_DFedAvg = np.zeros((len(metadataset), kwargs['nEpochs']+1))
     for ibatch in tqdm(range(len(metadataset))):
         iGraph = torch.tensor(Graph[ibatch], device=device).float()
         _, acc = DGD(metadataset[ibatch], iGraph, inner_loss, criterion, **kwargs)
+        _, acc2 = DSGD(metadataset[ibatch], iGraph, inner_loss, criterion, **kwargs)
+        _, acc3 = DFedAvg(metadataset[ibatch], iGraph, inner_loss, criterion, **kwargs)
         testAccuracy[ibatch] = acc
-    return testAccuracy
+        testAccuracy_DSGD[ibatch] = acc2
+        testAccuracy_DFedAvg[ibatch] = acc3
+    return testAccuracy, testAccuracy_DSGD, testAccuracy_DFedAvg
 
     
